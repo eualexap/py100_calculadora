9c34f3e8b457c67c8262f8b6790518f3aa30367b
Alex <alexalmeida.hg@gmail.com>
2026-01-12 19:06:45 -0300

    quarto commit

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from art import logo 
+
 def add(n1, n2):
     return n1 + n2
 
@@ -17,4 +19,17 @@ operations = {
     "*" : multi
 }
 
+print(logo)
 print(operations["+"](4, 3))
+
+"""
+Functionality
+Program asks the user to type the first number.
+Program asks the user to type a mathematical operator (a choice of "+", "-", "*" or "/")
+Program asks the user to type the second number.
+Program works out the result based on the chosen mathematical operator.
+Program asks if the user wants to continue working with the previous result.
+If yes, program loops to use the previous result as the first number and then repeats the calculation process.
+If no, program asks the user for the fist number again and wipes all memory of previous calculations.
+Add the logo from art.py
+"""
